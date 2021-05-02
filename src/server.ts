1f119d097eab430b5ca98a34b6963fbc4a107448
Jonathan Wollinger <jonathanwollinger@gmail.com>
2021-05-02 20:43:40 -0300

    feat: add new migrations files

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,10 @@ import './database';
 import cors from 'cors';
 import routes from './routes';
 
-
 const app = express();
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => console.log('Server Running'));
